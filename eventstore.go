dd3a455b14d565f4c2d7561334791b57eef06d55
unknown <287013373@qq.com>
2019-01-10 16:19:50 +0800

    add

--- eventstore.go
+++ eventstore.go
@@ -1,6 +1,6 @@
 package eventhus
 
-// EventStore saves the events from an aggregate
+// EventStore saves the events from an aggregate0000
 type EventStore interface {
 	Save(events []Event, version int) error
 	SafeSave(events []Event, version int) error
